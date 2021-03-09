2ab96577be9aff60e0d80e47d29a3af0393850b3
Nikolay Dobrev <ndobrev@progress.com>
2021-03-09 17:33:24 +0200

    migrate DB - create Comment model

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,10 +18,17 @@ func (a *App) Run() error {
 	fmt.Println("Setting Up Our App")
 
 	var err error
+
 	db, err := database.NewDatabase()
 	if err != nil {
 		return err
 	}
+
+	err = database.MigrateDB(db)
+	if err != nil {
+		return err
+	}
+
 	commentService := comment.NewService(db)
 
 	handler := transportHTTP.NewHandler(commentService)
